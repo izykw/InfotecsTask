9c7873586d040adc6c0873a085c02118065efb74
izykw <skytrixoff@gmail.com>
2023-01-29 23:45:35 +0300

    Добавлена дополнительная функциональность (Сортировка продуктов по заголовку и цене).

--- js/api.js
+++ js/api.js
@@ -1,3 +1,5 @@
 export function fetchData(count) {
-	return fetch(`https://dummyjson.com/products?limit=${count}`)
+	return (fetch(`https://dummyjson.com/products?limit=${count}`)
+		.then(res => res.json()))
+		.catch(err => console.error(err));
 }
--- js/main.js
+++ js/main.js
@@ -1,109 +1,143 @@
 import { fetchData } from "./api.js";
 
-let listItemsCount = 10;
-let sortBy = 'title';
-let data = [];
+const productsList = document.querySelector('.products_list');
+const productsCountElement = document.querySelector('.products_count');
+const productsCountButton = document.querySelector('.set_products_count');
+const productsSortElement = document.querySelector('.products_sort');
+const productInfo = document.createElement('div');
+productInfo.classList.add('product_info');
+const productImage = document.createElement('img');
+productImage.classList.add('product_image');
 
-function addListItems() {
-	fetchData(listItemsCount)
-		.then(res => res.json())
-		.then(({ products }) => {
-			// Сохраняем полученные продукты,
-			// чтобы при сортировке элементов не делать каждый раз новый запрос.
-			data = products;
-			const list = document.querySelector('.products_list');
+let productsCount = 10;
+let products = [];
 
-			// Очищаем предыдущий список элементов
-			list.innerHTML = '';
+productsList.addEventListener('mouseover', showProductInfo);
+productsList.addEventListener('mouseout', hideProductInfo);
+productsList.addEventListener('dragstart', (e) => {
+	e.target.classList.add('selected');
+});
+productsList.addEventListener('dragstart', (e) => {
+	e.target.classList.add('selected');
+});
+productsList.addEventListener('dragend', (e) => {
+	e.target.classList.remove('selected');
+});
+productsList.addEventListener('dragover', dragAndDrop);
+productsCountElement.addEventListener('keydown', (e) => {
+	if (e.key !== 'Enter') {
+		return;
+	}
 
-			products.forEach(({ id, title }) => {
-				const listItem = document.createElement('li');
-				listItem.classList.add('products_list_item');
-				listItem.id = id;
-				listItem.textContent = title;
-				list.appendChild(listItem);
-			})
-			dragAndDrop();
-		});
+	const count = Number(e.target.value);
+	setProductsCount(count);
+	addProductsToList();
+})
+productsCountButton.addEventListener('click', () => {
+	const count = Number(productsCountElement.value);
+	setProductsCount(count);
+	addProductsToList();
+})
+productsSortElement.addEventListener('change', (e) => {
+	sortProducts(e);
+	updateProductsList();
+})
+
+function addProductsToList() {
+	fetchData(productsCount).then(data => {
+		// Сохраняем полученные продукты,
+		// чтобы при сортировке элементов не делать каждый раз новый запрос.
+		products = data.products;
+		updateProductsList();
+	})
 }
 
-function showListItemInfo() {
-	const list = document.querySelector('.products_list');
-	const productInfoElement = document.querySelector('.products_info');
+function updateProductsList() {
+	// Очищаем предыдущий список элементов
+	productsList.innerHTML = '';
 
-	list.addEventListener('mouseover', (e) => {
-		const target = e.target;
-		if (target.tagName !== 'li'.toUpperCase()) {
-			return;
-		}
-		const id = Number(target.id);
-		const item = data.find((item) => item.id === id);
-		const { description, price, rating, brand, thumbnail } = item;
-		productInfoElement.innerText = `
+	products.forEach(({ id, title }) => {
+		const listItem = document.createElement('li');
+		listItem.classList.add('products_list_item');
+		listItem.id = id;
+		listItem.textContent = title;
+		productsList.appendChild(listItem)
+	});
+}
+
+function showProductInfo(e) {
+	const target = e.target;
+	if (target.tagName !== 'li'.toUpperCase()) {
+		return;
+	}
+
+	const id = Number(target.id);
+	const item = products.find((item) => item.id === id);
+	const { description, price, rating, brand, thumbnail } = item;
+
+	productInfo.innerText = `
 				brand: ${brand}
 				rating: ${rating}
 				price: ${price}
 				description: ${description}
 			`;
-		const productImage = document.createElement('img');
-		productImage.classList.add('products_image');
-		productImage.src = thumbnail;
-		productInfoElement.appendChild(productImage);
-		productInfoElement.classList.add('show');
-	})
+	productImage.src = thumbnail;
+	productInfo.appendChild(productImage);
+	productInfo.classList.add('show');
+	target.appendChild(productInfo);
 
-	list.addEventListener('mouseout', () => {
-		productInfoElement.classList.remove('show');
-	})
+
+	const image = document.createElement('img');
+	image.classList.add('product_image');
+
+	const element = document.createElement('div');
+	element.classList.add('product_info');
+}
+
+function hideProductInfo() {
+	productInfo.classList.remove('show');
 }
 
 // Не стал использовать события mousedown, mousemove and mouseup,
 // так как посчитал, что встроенного api достаточно для данной задачи.
-function dragAndDrop() {
-	const list = document.querySelector('.products_list');
+function dragAndDrop(e) {
+	e.preventDefault();
+
+	setListItemsDraggable();
+
+	const activeElement = productsList.querySelector('.selected');
+	const currentElement = e.target;
+
+	// Проверяем, что событие сработало:
+	// 1. Именно на элементе списка
+	// 2. И не на том элементе, который мы перемещаем
+	const isMovable = activeElement !== currentElement &&
+		currentElement.classList.contains('products_list_item');
+
+	if (!isMovable) {
+		return;
+	}
+
+	const nextElement = getNextElement(e.clientY, currentElement);
+
+	if (nextElement &&
+		activeElement === nextElement.previousElementSibling ||
+		activeElement === nextElement) {
+		return;
+	}
+
+	productsList.insertBefore(activeElement, nextElement);
+}
+
+function setListItemsDraggable() {
 	const listItems = document.querySelectorAll('.products_list_item');
 
 	listItems.forEach(item => {
 		item.draggable = true;
 	})
-
-	list.addEventListener('dragstart', (e) => {
-		e.target.classList.add('selected');
-	})
-
-	list.addEventListener('dragend', (e) => {
-		e.target.classList.remove('selected');
-	})
-
-	list.addEventListener('dragover', (e) => {
-		e.preventDefault();
-
-		const activeElement = list.querySelector('.selected');
-		const currentElement = e.target;
-
-		// Проверяем, что событие сработало:
-		// 1. Именно на элементе списка
-		// 2. И не на том элементе, который мы перемещаем
-		const isMovable = activeElement !== currentElement &&
-			currentElement.classList.contains('products_list_item');
-
-		if (!isMovable) {
-			return;
-		}
-
-		const nextElement = getNextElement(e.clientY, currentElement);
-
-		if (nextElement &&
-			activeElement === nextElement.previousElementSibling ||
-			activeElement === nextElement) {
-			return;
-		}
-
-		list.insertBefore(activeElement, nextElement);
-	})
 }
 
-function getNextElement(cursorPosition, currentElement) {
+export function getNextElement(cursorPosition, currentElement) {
 	const currentElementCoordinates = currentElement.getBoundingClientRect();
 	const currentElementCenter = currentElementCoordinates.y +
 		currentElementCoordinates.height / 2;
@@ -114,32 +148,42 @@ function getNextElement(cursorPosition, currentElement) {
 		currentElement.nextElementSibling;
 }
 
-function getProducts() {
-	const productsCountInput = document.querySelector('.products_count');
-	productsCountInput.addEventListener('keydown', (e) => {
-		if(e.key !== 'Enter') {
-			return;
-		}
-
-		const productsCount = e.target.value;
-		if(productsCount < 0 || productsCount > 100) {
-			alert('Количество выводимых элементов должно быть в диапазоне от 0 до 100');
-			return;
-		}
-
-		listItemsCount = Number(productsCount);
-		addListItems(listItemsCount);
-	})
+function setProductsCount(count) {
+	if (count < 0 || count > 100) {
+		alert('Количество выводимых элементов должно быть в диапазоне от 0 до 100');
+		return;
+	}
+	productsCount = count;
 }
 
-function sortProducts() {
-	const sortProductsSelect = document.querySelector('.products_sort');
-	sortProductsSelect.addEventListener('change', (e) => {
-		sortBy = e.target.value;
-	})
+function sortProducts(e) {
+	const option = e.target.value;
+
+	switch (option) {
+		case 'title': {
+			products = products.sort((a,b) => {
+				// Для сортировки по алфавиту, чтобы не учитывался регистр.
+				const firstTitle = a.title.toLowerCase();
+				const secondTitle = b.title.toLowerCase();
+
+				if(firstTitle > secondTitle) {
+					return 1;
+				}
+				if(firstTitle < secondTitle) {
+					return -1;
+				}
+				return 0;
+			});
+			break;
+		}
+		case 'price': {
+			products = products.sort((a, b) =>  a.price - b.price);
+			break;
+		}
+		default: {
+			console.log('There is no such case.')
+		}
+	}
 }
 
-addListItems();
-showListItemInfo();
-getProducts();
-sortProducts();
+addProductsToList();
